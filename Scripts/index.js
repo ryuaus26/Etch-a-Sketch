c440f77da0649997fbcc32277dd4c6c5c4e9142d
ryuaus26 <yunryu1217@gmail.com>
2023-01-09 20:51:42 -0700

    Updated

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,6 +1,10 @@
 document.addEventListener( "DOMContentLoaded",function(){
+   
+});
+
+function createBoard(size){
     let board = document.querySelector('.board');
 
     board.style.gridTemplateColumn = "repeat(16,1fr)";
     border.style.gridTemplateColumn= "repeat(16,1fr)";
-});
+}
