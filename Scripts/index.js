97e0cd2d59e7d25c9c488745dbb6f6ccf2c25790
ryuaus26 <yunryu1217@gmail.com>
2023-01-09 20:44:39 -0700

    Added an attempt of tyring to implement 16x16 div

---
+++ Scripts/index.js
@@ -0,0 +1,6 @@
+document.addEventListener( "DOMContentLoaded",function(){
+    let board = document.querySelector('.board');
+
+    board.style.gridTemplateColumn = "repeat(16,1fr)";
+    border.style.gridTemplateColumn= "repeat(16,1fr)";
+});
